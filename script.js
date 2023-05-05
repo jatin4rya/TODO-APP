5d7256385c531e59d7f9f76941ba52e313bcb31c
Jatin1007 <Jatin4201@outlook.com>
2023-05-05 10:42:15 +0530

    05-05-2023

--- script.js
+++ script.js
@@ -2,13 +2,15 @@ const addItemButton = document.getElementById('add-item');
 const newItemInput = document.getElementById('new-item');
 const todoList = document.getElementById('todo-list');
 const body = document.querySelector('body')
+const trashCan = document.getElementById('trashCan').innerHTML
 
-addItemButton.addEventListener('click', () => {
+function addItem() {
     const newItemText = newItemInput.value;
     const newItem = document.createElement('li');
     newItem.innerText = newItemText;
     const checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
+
     checkbox.addEventListener('change', () => {
         if (checkbox.checked) {
             newItem.style.textDecoration = 'line-through';
@@ -18,7 +20,25 @@ addItemButton.addEventListener('click', () => {
             newItem.style.color = body.style.color;
         }
     });
+
+    const dlt = document.createElement('button');
+    dlt.innerHTML = trashCan;
+    dlt.classList.add("dltKey")
+    dlt.addEventListener('click', () => {
+        newItem.style.display = 'none';
+    })
+
+
     newItem.prepend(checkbox);
+    newItem.prepend(dlt);
     todoList.appendChild(newItem);
     newItemInput.value = '';
+}
+
+addItemButton.addEventListener('click', addItem);
+
+newItemInput.addEventListener('keyup', (event) => {
+    if (event.key === 'Enter') {
+        addItem();
+    }
 });
